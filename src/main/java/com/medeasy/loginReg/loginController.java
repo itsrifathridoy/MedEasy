26fb151b03d8155cf89d2d376328e738b3eab8ca
Jiyasmin Sinthiya <jaksinthi@gmail.com>
2023-03-04 07:03:20 +0600

    hover effect added

--- src/main/java/com/medeasy/loginReg/loginController.java
+++ src/main/java/com/medeasy/loginReg/loginController.java
@@ -3,16 +3,29 @@ package com.medeasy.loginReg;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class loginController {
+public class loginController implements Initializable {
 
+
+    public ImageView maximize;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        maximize.setOpacity(0.5);
+    }
 
     public void signup(ActionEvent ae) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("regBirth.fxml"));
@@ -21,4 +34,15 @@ public class loginController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void close(MouseEvent me) {
+        Stage stage = (Stage) ((Node) me.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void minimize(MouseEvent me) {
+        Stage stage = (Stage) ((Node) me.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
 }
